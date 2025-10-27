151b99342d12ea98800f1f9ec72c8e9d04657146
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-10-27 22:00:19 +0900

    feat: 경주 상수 및 메시지 상수 추가
    - Message.java: 사용자 메시지 상수
    - RacingConstants.java: 경주 규칙 상수

---
+++ src/main/java/racingcar/constants/Message.java
@@ -0,0 +1,9 @@
+package racingcar.constants;
+
+public class Message {
+    public static final String CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String TRY_COUNT_PROMPT = "시도할 횟수는 몇 회인가요?";
+    public static final String EXECUTION_RESULT = "실행 결과";
+    public static final String WINNER_PREFIX = "최종 우승자 : ";
+}
+
---
+++ src/main/java/racingcar/constants/RacingConstants.java
@@ -0,0 +1,10 @@
+package racingcar.constants;
+
+public class RacingConstants {
+    public static final int MIN_MOVE_THRESHOLD = 4;
+    public static final int MIN_RANDOM_VALUE = 0;
+    public static final int MAX_RANDOM_VALUE = 9;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static final int MIN_TRY_COUNT = 1;
+}
+
